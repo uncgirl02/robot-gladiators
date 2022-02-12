bb18bc021b6b955bf330f9eae0b41d131169af6c
Erin Ewart <ems1980@hotmail.com>
2022-02-12 14:31:54 -0500

    added random order to fight sequence

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,14 +36,24 @@ var fightOrSkip = function() {
   }
 }
 
+
+
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+      isPlayerTurn = false;
+    } 
+
     // repeat and execute as long as the enemy-robot is alive 
     while (playerInfo.health > 0 && enemy.health > 0) {
-       // ask player if they'd like to fight or skip using fightOrSkip function
-    if (fightOrSkip()) {
-      // if true, leave fight by breaking loop
-      break;
+      if (isPlayerTurn) {
+        // ask player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
     }
 
     // generate random damage value based on player's attack power
@@ -85,6 +95,9 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  // switch turn order for next round
+  isPlayerTurn = !isPlayerTurn;
+}
 };
 
 // function to start a new game
